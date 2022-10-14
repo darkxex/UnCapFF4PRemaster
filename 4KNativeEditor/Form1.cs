8907ace3e1ea99d44f0a656c1fb63dacdf7d2de6
Angel Xex <darkxex@gmail.com>
2022-10-14 20:53:51 -0300

    sure

--- 4KNativeEditor/Form1.cs
+++ 4KNativeEditor/Form1.cs
@@ -47,7 +47,7 @@ namespace _4KNativeEditor
            
             using (FileStream sr = File.OpenWrite(@"GameAssembly.dll"))
             {
-                sr.Seek(0x4B173A, SeekOrigin.Begin);
+                sr.Seek(0xE49F6A, SeekOrigin.Begin);
                 sr.WriteByte((byte)Width);
        
                
